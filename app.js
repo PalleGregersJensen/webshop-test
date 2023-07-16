a63c76da363b8e44bd7b99d11ec33f6b52470683
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-07-16 23:14:26 +0200

    De første indledende step til en webshop er taget,

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 "use strict"
 
+let items = [];
+
 window.addEventListener("load", start);
 
-function start() {
+async function start() {
     console.log("JS kører");
+    items = await getJsonFile();
+    console.log(items);
+    showItems(items);
+}
+
+async function getJsonFile() {
+    const response = await fetch ("list-of-items.json");
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+function showItems(beerList) {
+  for (const beer of beerList) {
+    const beerHtml = /*html*/ `<li>${beer.name}, <img src="${beer.image}">, ${beer.description}, ${beer.price}</li>`;
+    document.querySelector("#items-list").insertAdjacentHTML("beforeend", beerHtml);
+  }
 }
