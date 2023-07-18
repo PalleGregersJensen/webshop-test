d66b6836296d82498fe13b06174d307b92acd261
PalleGregersJensen <palle.gregers.jensen@gmail.com>
2023-07-18 12:02:05 +0200

    Filter-funktion implementeret, men skal finpudses.

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ async function start() {
   console.log("JS kører");
   items = await getJsonFile();
   console.log(items);
-    showItems(items);
+  showItems(items);
+  document.querySelector("#liquor-checkbox").addEventListener("change", filterByLiquor);
+  document.querySelector("#beer-checkbox").addEventListener("change", filterByBeer);
   document.querySelector("#sort-by").addEventListener("change", handleSortBy);
   document.querySelector("#search-field").addEventListener("keyup", searchFunction);
 }
@@ -22,7 +24,7 @@ async function getJsonFile() {
 }
 
 function showItems(beerList) {
-    document.querySelector("#items-list").innerHTML = "";
+  document.querySelector("#items-list").innerHTML = "";
   for (const beer of beerList) {
     const beerHtml = /*html*/ `<p>${beer.name} <br>
     <img src=${beer.image} alt="beer.caption"/> <br> 
@@ -33,26 +35,49 @@ function showItems(beerList) {
 }
 
 function handleSortBy() {
-    const sortBy = document.querySelector("#sort-by").value;
-    if (sortBy === "low-to-high") {
-        console.log("low-to-high");
-        items.sort((a, b) => a.price - b.price)
-        showItems(items);
-    } else if (sortBy === "high-to-low") {
-        console.log("high-to-low");
-        items.sort((a, b) => b.price - a.price);
-        showItems(items);
-    } else if (sortBy === "alphabetical") {
-        console.log("alphabetical");
-        items.sort((a, b) => a.name.localeCompare(b.name));
-        showItems(items);
-    }
+  const sortBy = document.querySelector("#sort-by").value;
+  if (sortBy === "low-to-high") {
+    console.log("low-to-high");
+    items.sort((a, b) => a.price - b.price);
+    showItems(items);
+  } else if (sortBy === "high-to-low") {
+    console.log("high-to-low");
+    items.sort((a, b) => b.price - a.price);
+    showItems(items);
+  } else if (sortBy === "alphabetical") {
+    console.log("alphabetical");
+    items.sort((a, b) => a.name.localeCompare(b.name));
+    showItems(items);
+  }
 }
 
 function searchFunction() {
   let searchValue = document.querySelector("#search-field").value;
   searchValue = searchValue.toLowerCase();
   console.log(searchValue);
-  
-  // showItems(searchValue);
 }
+
+function filterByBeer() {
+  if (document.querySelector("#beer-checkbox").checked) {
+    console.log("beer checked");
+    const result = items.filter(checkForBeer);
+    showItems(result);
+  }
+}
+
+function filterByLiquor() {
+  if (document.querySelector("#liquor-checkbox").checked) {
+    console.log("liquor checked");
+    const result = items.filter(checkForLiquor);
+    showItems(result);
+  }
+}
+
+function checkForLiquor(items) {
+  return items.type === "liquor";
+}
+
+function checkForBeer(items) {
+  return items.type === "beer";
+}
+
